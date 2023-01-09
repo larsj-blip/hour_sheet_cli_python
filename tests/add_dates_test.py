6a68a4b764d30e00c1e3a122017c92dcba847c2b
lars jaatun <lars.jaatun@adresseavisen.no>
2023-01-09 08:50:34 +0100

    added tests and spaghetti

--- tests/add_dates_test.py
+++ tests/add_dates_test.py
@@ -1,14 +1,10 @@
-
+from __future__ import annotations
 from assertpy import assert_that
 from src.hour_sheet import hourSheet
 import datetime
 
 
-
-CURRENT_DAY = datetime.datetime.now().day
-CURRENT_MONTH = datetime.datetime.now().month
-START_TIME_AS_INT = 800
-END_TIME_AS_INT = 1800
+from .conftest import CURRENT_DAY, CURRENT_MONTH
 
 
 
@@ -20,17 +16,13 @@ def should_save_start_time_for_workday_from_int_representations_of_date_and_time
     list_of_days = temp_hour_sheet.most_recent_day()
     assert_that(list_of_days).is_not_empty()
 
-# def should_not_create_start_or_end_of_workday_if_it_already_exists_for_specified_date(temp_hour_sheet_with_start_date):
-#     hour_sheet = temp_hour_sheet_with_start_date
-#     assert_that(hour_sheet.start_day(START_TIME_AS_INT, CURRENT_DAY, CURRENT_MONTH)).raises(RuntimeError)
+def should_not_implicitly_overwrite_existing_entry_(temp_hour_sheet_with_start_date : hourSheet):
+    hour_sheet = temp_hour_sheet_with_start_date
+    hour_sheet.end_day(1600, CURRENT_DAY, CURRENT_MONTH)
+    expected_hour_summary = hour_sheet.get_summary_for_date(day=CURRENT_DAY, month=CURRENT_MONTH)
+    hour_sheet.start_day(1800, current_day_as_int=CURRENT_DAY, current_month_as_int=CURRENT_MONTH)
+    assert_that(hour_sheet.get_summary_for_date(CURRENT_DAY, CURRENT_MONTH)).is_equal_to(expected_hour_summary)
 
-def should_save_end_of_workday_object_together_with_start_of_workday_object(
-    hour_sheet_with_full_workday
-):
-    temp_hour_sheet = hour_sheet_with_full_workday
-    most_recent_day = temp_hour_sheet.most_recent_day()
-    assert_that(most_recent_day).contains_key("start")
-    assert_that(most_recent_day).contains_key("end")
 
 def should_differentiate_between_workdays_from_different_months(hour_sheet_with_full_workday):
     hour_sheet = hour_sheet_with_full_workday
