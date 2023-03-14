65a44a2ce751882a3dbd9d64c96be7fcd677df6d
lars jaatun <lars.jaatun@adresseavisen.no>
2023-03-14 17:27:32 +0100

    wip cli writing tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,6 +1,20 @@
+from dataclasses import dataclass
+from unittest.mock import patch
 from assertpy import assert_that
+from src.cli import hour_sheet_utils
+from src.hour_sheet import hourSheet
+from tests.IO_test import TEST_FILE_NAME
 
+START_TIME = 900
 
+@dataclass
+class mock_namespace:
+    entry_type: str
+    time: int
 
-def should_start_day_given_command_line_args_with_start_flag_and_int_corresponding_to_time():
-    pass
+@patch.object(hour_sheet.hourSheet, "start_today")
+def should_start_day_given_command_line_args_with_start_flag_and_int_corresponding_to_time(mocked_hoursheet):
+    start_day_input = mock_namespace(entry_type="start", time=START_TIME)
+    hour_sheet_utils.add_entry_to_hoursheet(mocked_hoursheet, start_day_input, TEST_FILE_NAME)
+    mocked_hoursheet.start_today.assert_called_with(START_TIME)
+    
